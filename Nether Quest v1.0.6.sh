366282103546fd4508b54c1f76791d19c3afcf9f
FerrariBulletA380 <119022143+FerrariBulletA380@users.noreply.github.com>
2022-11-27 10:25:02 +0800

    Add files via upload

---
+++ Nether Quest v1.0.6.sh
@@ -0,0 +1,14 @@
+echo Install Nether Quest
+echo 
+read -p "Press any key to continue"
+echo Downloading files...
+gnome-open https://drive.google.com/u/0/uc?id=11Fz6pu85K9cWsim9l828soeY2jX576bi&export=download&confirm=t&uuid=81d7d6c6-5f84-4e18-bb49-8be5ac9c3d45&at=AHV7M3ewR83Sg3gDzrBVyTLRwxdZ:1669342824656
+kde-open https://drive.google.com/u/0/uc?id=11Fz6pu85K9cWsim9l828soeY2jX576bi&export=download&confirm=t&uuid=81d7d6c6-5f84-4e18-bb49-8be5ac9c3d45&at=AHV7M3ewR83Sg3gDzrBVyTLRwxdZ:1669342824656
+xdg-open https://drive.google.com/u/0/uc?id=11Fz6pu85K9cWsim9l828soeY2jX576bi&export=download&confirm=t&uuid=81d7d6c6-5f84-4e18-bb49-8be5ac9c3d45&at=AHV7M3ewR83Sg3gDzrBVyTLRwxdZ:1669342824656
+python -m webbrwoser https://drive.google.com/u/0/uc?id=11Fz6pu85K9cWsim9l828soeY2jX576bi&export=download&confirm=t&uuid=81d7d6c6-5f84-4e18-bb49-8be5ac9c3d45&at=AHV7M3ewR83Sg3gDzrBVyTLRwxdZ:1669342824656
+echo Zip file downloaded...
+read -p "Press any key to continue"
+echo Please extract the files in the zip file to your preferred location
+read -p "Press any key to continue"
+echo Run start.sh to start the game
+read -p "Press any key to continue"
